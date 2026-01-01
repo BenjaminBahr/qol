3ce9e2a141a187f46c8625d623a86d18015f56a0
Benjamin Bahr <bbahr26@gmail.com>
2026-01-01 22:15:23 +0100

    Refactor partial_sum

--- src/arithmetics.rs
+++ src/arithmetics.rs
@@ -1,3 +1,5 @@
+use std::ops::Add;
+
 pub mod have_same_sign;
 pub mod partial_sum;
 
@@ -23,3 +25,24 @@ where
         || ( a == zero && b == zero)
 }
 
+/// Examples:
+/// ```
+/// use crate::bb_qol::arithmetics::partial_sum;
+///
+/// let vector_of_numbers: Vec<i32> = vec![1, 2, 0, -4, -5];
+/// let partial_sums = partial_sum(&vector_of_numbers);
+///
+/// assert_eq!(partial_sums, vec![1, 3, 3, -1, -6]);
+/// ```
+pub fn partial_sum<T>(n: &[T]) -> Vec<T>
+where
+    T: Add<Output = T> + Default + Copy
+{
+    let zero = Default::default();
+    n.iter()
+        .scan(zero, |sum, &x| {
+            *sum = *sum + x; // Update the running total
+            Some(*sum) // Yield the current total
+        })
+        .collect()
+}

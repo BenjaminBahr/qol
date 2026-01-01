ed5cd2565b891bee72def28935193083db976132
Benjamin Bahr <bbahr26@gmail.com>
2026-01-01 22:06:57 +0100

    Refactor have_same_sign method

--- src/arithmetics.rs
+++ src/arithmetics.rs
@@ -1,2 +1,25 @@
 pub mod have_same_sign;
 pub mod partial_sum;
+
+/// Examples:
+/// ```
+/// use crate::bb_qol::arithmetics::have_same_sign;
+/// let a: i32 = 2;
+/// let b: i32 = 3;
+/// let c: f64 = 0.0;
+/// let d: f64 = -3.14;
+/// assert!(have_same_sign(a, b));
+/// assert!(!have_same_sign(c, d));
+/// assert!(!have_same_sign(1, 0));
+/// assert!(have_same_sign(0, 0))
+/// ```
+pub fn have_same_sign<T>(a: T, b: T) -> bool
+where
+    T: PartialOrd + Default
+{
+    let zero: T = Default::default();
+    ( a > zero && b > zero )
+        || ( a < zero && b < zero )
+        || ( a == zero && b == zero)
+}
+
